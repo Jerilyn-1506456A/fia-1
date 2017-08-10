a4df3e1e3fc5272e4632dd8d6b1538f34d8a551c
Jerilyn-1506456A <1506456A@student.tp.edu.sg>
2017-08-11 00:51:45 +0800

    Update 6.2.1.10.sh

--- 6.2.1.10.sh
+++ 6.2.1.10.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #6.2.1.10
 printf "Checking if login and logout events are recorded: "
 if (grep logins /etc/audit/audit.rules | grep "w /var/log/faillog -p wa -k logins" >/dev/null) && (grep logins /etc/audit/audit.rules | grep "w /var/log/lastlog -p wa -k logins" >/dev/null) && (grep logins /etc/audit/audit.rules | grep "w /var/log/tallylog -p wa -k logins" >/dev/null); then
