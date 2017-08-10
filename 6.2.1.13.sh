fc710c9b1f66e57f695592da93e5caf8ae8c8c19
Jerilyn-1506456A <1506456A@student.tp.edu.sg>
2017-08-10 23:55:52 +0800

    Update 6.2.1.13.sh

--- 6.2.1.13.sh
+++ 6.2.1.13.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #6.2.1.13
 printf "Checking if there are unsuccessful attempts: "
 if (grep access /etc/audit/audit.rules | grep "a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access" >/dev/null) && (grep access /etc/audit/audit.rules | grep "a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access" >/dev/null) && (grep access /etc/audit/audit.rules | grep "a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access" >/dev/null) && (grep access /etc/audit/audit.rules | grep "a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access" >/dev/null) ; then
