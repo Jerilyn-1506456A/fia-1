e506675a5c2c433da799582ee9984a6b66b8f291
Jerilyn-1506456A <1506456A@student.tp.edu.sg>
2017-08-10 23:56:03 +0800

    Update 6.2.1.14.sh

--- 6.2.1.14.sh
+++ 6.2.1.14.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #6.2.1.14
 printf "Checking if privileged commands are in audit: "
 find / -xdev \( -perm -4000 -o -perm -2000 \) -type f | awk '{print "-a always,exit-F path=" $1 " -F perm=x -F auid>=1000 -F auid!=4294967295 -k privileged" }' > /tmp/1.log
