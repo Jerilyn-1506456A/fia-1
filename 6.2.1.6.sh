3e57b12283f5f2197f242a3f6bc12fba3f563fbf
Jerilyn-1506456A <1506456A@student.tp.edu.sg>
2017-08-10 23:59:05 +0800

    Update 6.2.1.6.sh

--- 6.2.1.6.sh
+++ 6.2.1.6.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #6.2.1.6
 printf "Checking if system date/time are captured when modified: "
 if (egrep 'adjtimex|settimeofday|clock_settime' /etc/audit/audit.rules | grep "a always, exit -F arch=b64 -S adjtimex -S settimeofday -k time-change" >/dev/null) && (egrep 'adjtimex|settimeofday|clock_settime' /etc/audit/audit.rules | grep "a always, exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change" >/dev/null) && (egrep 'adjtimex|settimeofday|clock_settime' /etc/audit/audit.rules | grep "a always, exit -F arch=b64 -S clock_settime -k time-change" >/dev/null) && (egrep 'adjtimex|settimeofday|clock_settime' /etc/audit/audit.rules | grep "a always, exit -F arch=b32 -S clock_settime -k time-change" >/dev/null) ; then
